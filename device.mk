83d6aebc2428c48801027fca14d26240625a8915
DarkBlood23 <gabro2003@gmail.com>
2016-09-05 15:10:16 +0300

    Revert apt 1 fix

--- device.mk
+++ device.mk
@@ -166,10 +166,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Torch
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # STk
 PRODUCT_PACKAGES += \
     Stk
